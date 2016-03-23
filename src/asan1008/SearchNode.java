a1fe50ca735e3b79e82c121b0fba868023bd9584
jnasante <josephnasante@yahoo.com>
2016-03-23 15:02:59 -0500

    Incorporated running Example A* Client

---
+++ src/asan1008/SearchNode.java
@@ -0,0 +1,23 @@
+package asan1008;
+
+
+public class SearchNode {
+	Vertex vertex;
+	Edge edge;
+	
+	public SearchNode(Vertex v, Edge e) {
+		vertex = v;
+		edge = e;
+	}
+
+	public Edge getEdge() {
+		return edge;
+	}
+
+	public Vertex getVertex() {
+		return vertex;
+	}
+	
+	
+
+}
