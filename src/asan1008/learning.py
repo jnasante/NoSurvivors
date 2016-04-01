c3568b5fa154cf85340d8a5b161b84c871c3c836
jnasante <josephnasante@yahoo.com>
2016-03-31 23:07:32 -0500

    Wrote python script to graph learning

---
+++ src/asan1008/learning.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plot
+import pandas as pd
+
+# Read file
+data = pd.read_csv('learning.csv')
+scores = data["score"]
+
+# Draw graph
+fig, axes = plot.subplots(nrows=1, ncols=1)
+axes.plot(range(1, len(scores)+1), scores)
+axes.set_title('Generations vs. Fitness')
+axes.set_xlabel('Generations')
+axes.set_ylabel('Fitness')
+plot.show()
+
